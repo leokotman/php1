18c60dda31517bb9375688fe6854375b8c14aea9
levkotman <leoktmn@yandex.ru>
2021-01-31 21:06:54 +0300

    mistakes corrected

--- les2/task1-7(ex5)/index.php
+++ les2/task1-7(ex5)/index.php
@@ -11,107 +11,108 @@ if ($a >= 0 && $b >= 0){
 } else {
     echo $a + $b;
 }
+
 echo "<br>";
+
 
 //task 2
 echo "task 2 <br>";
 $a = random_int(0, 15);
 switch ($a){
-    case (0):
-        echo 0 . "<br>";
-    case (1):
-        echo 1 . "<br>";
-    case (2):
-        echo 2 . "<br>";
-    case (3):
-        echo 3 . "<br>";
-    case (4):
-        echo 4 . "<br>";
-    case (5):
-        echo 5 . "<br>";
-    case (6):
-        echo 6 . "<br>";
-    case (7):
-        echo 7 . "<br>";
-    case (8):
-        echo 8 . "<br>";
-    case (9):
-        echo 9 . "<br>";
-    case (10):
-        echo 10 . "<br>";
-    case (11):
-        echo 11 . "<br>";
-    case (12):
-        echo 12 . "<br>";
-    case (13):
-        echo 13 . "<br>";
-    case (14):
-        echo 14 . "<br>";
-    case (15):
-        echo 15 . "<br>";
+    case 0:
+        echo $a++ . "<br>";
+    case 1:
+        echo $a++ . "<br>";
+    case 2:
+        echo $a++ . "<br>";
+    case 3:
+        echo $a++ . "<br>";
+    case 4:
+        echo $a++ . "<br>";
+    case 5:
+        echo $a++ . "<br>";
+    case 6:
+        echo $a++ . "<br>";
+    case 7:
+        echo $a++ . "<br>";
+    case 8:
+        echo $a++ . "<br>";
+    case 9:
+        echo $a++ . "<br>";
+    case 10:
+        echo $a++ . "<br>";
+    case 11:
+        echo $a++ . "<br>";
+    case 12:
+        echo $a++ . "<br>";
+    case 13:
+        echo $a++ . "<br>";
+    case 14:
+        echo $a++ . "<br>";
+    case 15:
+        echo $a++ . "<br>";
 }
 
 //task 3
-$a = rand(1, 10);
-$b = rand(1, 10);
+$a = rand(0, 10);
+$b = rand(0, 10);
 echo "task 3: a = {$a}, b = {$b} <br>";
 
 function add($x, $y){
     return $x + $y;
 }
-echo "сумма: " . add($a, $b) . "<br>";
-
 function subtract($x, $y){
     return $x - $y;
 }
-echo "разность: " . subtract($a, $b) . "<br>";
-
 function multiply($x, $y){
     return $x * $y;
 }
+function divide($x, $y){
+    if ($y != 0){
+        return $x / $y;
+    } else {
+        return '"Ошибка: на ноль делить нельзя"';
+    }
+
+}
+echo "сумма: " . add($a, $b) . "<br>";
+
+echo "разность: " . subtract($a, $b) . "<br>";
+
 echo "произведение: " . multiply($a, $b) . "<br>";
 
-function divide($x, $y){
-    return $x / $y;
-}
 echo "деление: " . divide($a, $b) . "<br>";
 
 //task 4
 echo "task 4 <br>";
 function mathOperation($x, $y, $operation){
     switch ($operation){
-        case ("сумма"):
-            echo "сумма: " . add($x, $y);
-            break;
-        case ("разность"):
-            echo "разность: " . subtract($x, $y);
-            break;
-        case ("произведение"):
-            echo "произведение: " . multiply($x, $y);
-            break;
-        case ("деление"):
-            echo "деление: " . divide($x, $y);
-            break;
+        case "сумма":
+            return add($x, $y);
+        case "разность":
+            return subtract($x, $y);
+        case "произведение":
+            return multiply($x, $y);
+        case "деление":
+            return divide($x, $y);
         default:
-            echo "Неверные значения функции";
+            return "Неверные значения функции";
     }
 }
-mathOperation(3,5,'сумма');
-mathOperation(3,5,'разность');
-mathOperation(3,5,'произведение');
-mathOperation(3,5,'деление');
+echo "сумма: " . mathOperation(3,5,'сумма') . "<br>";
+echo "разность: " . mathOperation(3,5,'разность') . "<br>";
+echo "произведение: " . mathOperation(3,5,'произведение') . "<br>";
+echo "деление: " . mathOperation(3,5,'деление') . "<br>";
 
 //task 6
-echo "<br> task 6 <br>";
+echo "task 6 <br>";
 function power($val, $pow){
-    for ($val != 0; (int) $pow >= 0; $pow--){
-        if ($pow == 0){
-            return 1;
-        } elseif ($pow == 1){
-            return $val;
-        } else {
-            return $val * power($val, $pow - 1);
-        }
+    if ($pow == 0){
+        return 1;
+    } elseif ($pow == 1){
+        return $val;
+    } else {
+        return $val * power($val, $pow - 1);
     }
 }
 echo power(3,3) . "<br>";
@@ -139,5 +140,4 @@ function currentTime(){
     }
     return $time;
 }
-echo "Текущее время (по серверу): " . currentTime();
-
+var_dump("Текущее время (по серверу): " . currentTime());
